5b81d381a2a3c1944c0f1c595fb83c66cb73930d
M. Algarni <m.t.algarni@gmail.com>
2017-03-14 15:30:30 +0300

    office 14-03-2017

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,31 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AngularFireModule } from 'angularfire2';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LessonsService } from 'app/share/model/lessons.service';
 import { firebaseConfig } from 'environments/firebase.config';
+import { LessonsListComponent } from './lessons-list/lessons-list.component';
+import { routerConfig } from './router.config';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LessonsListComponent,
+    TopMenuComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(firebaseConfig),
+    RouterModule.forRoot(routerConfig)
   ],
   providers: [LessonsService],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,7 +10,8 @@ import { Lesson } from '../share/model/lesson';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  lessons: Lesson[];
+  AllLessons: Lesson[];
+  filtered: Lesson[];
 
   constructor(private lessonsService: LessonsService) { }
 
@@ -18,8 +19,12 @@ export class HomeComponent implements OnInit {
     this.lessonsService.findAllLessons()
       .do(console.log)
       .subscribe(
-      lessons => this.lessons = lessons
+      lessons => this.AllLessons = this.filtered = lessons
       );
+  }
+
+  search(search: string){
+    this.filtered = this.AllLessons.filter(lesson => lesson.description.includes(search));
   }
 
 }
